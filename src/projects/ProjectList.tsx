ed8c277ddcfed3536887c9f136c476966282faf7
Tiela Black-Law <tiela.rose@gmail.com>
2023-11-08 18:27:40 -0800

    Create ProjectCard component

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types";
 import { Project } from "./Project";
+import ProjectCard from "./ProjectCard";
 
 interface ProjectListProps {
   projects: Project[];
@@ -10,24 +10,13 @@ interface ProjectListProps {
 // }
 
 export default function ProjectList({ projects }: ProjectListProps) {
-  return (
-    <div className="row">
-      {projects.map((project) => (
-        <div className="cols-sm" key={project.id}>
-          <div className="card">
-            <img src={project.imageUrl} alt={project.name} />
-            <section className="section dark">
-              <h5 className="strong">
-                <strong>{project.name}</strong>
-              </h5>
-              <p>{project.description}</p>
-              <p>Budget: ${project.budget.toLocaleString()}</p>
-            </section>
-          </div>
-        </div>
-      ))}
+  const items = projects.map((project) => (
+    <div className="cols-sm" key={project.id}>
+      <ProjectCard project={project} />
     </div>
-  );
+  ));
+
+  return <div className="row">{items}</div>;
 }
 
 // {
